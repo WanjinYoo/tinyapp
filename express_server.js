c097a2dceb8e1cab02189dad2f5bd6c1e00752fc
Wanjin Yoo <dksladjsla@gmail.com>
2020-11-10 05:19:20 +0000

    random string generator function added to express_server.js

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -6,14 +7,35 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const generateRandomString = () => {
+  let s = "";
+  for (let i = 0; i < 6; i++) {
+    let character = Math.floor(Math.random() * 25 + 65); // uppercase
+    if (Math.floor(Math.random() * 2 + 1) === 1) {
+      s += String.fromCharCode(character).toLowerCase();
+    } else {
+      s += String.fromCharCode(character);
+    }
+    
+  }
+  return s;
+};
+generateRandomString();
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   console.log(templateVars);
   res.render("urls_index", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
